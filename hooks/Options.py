f6790787f16d4d139849df76b3b12d587352d0d5
BuildTools <unconfigured@null.spigotmc.org>
2024-01-26 20:47:56 -0600

    made Timetrial and Cups work, also increased the trophy count and victoru locations up to 500 to account for incrrased potential trophy requirements

--- hooks/Options.py
+++ hooks/Options.py
@@ -26,6 +26,33 @@ from ..Helpers import is_option_enabled, get_option_value
 # To add an option, use the before_options_defined hook below and something like this:
 #   options["total_characters_to_win_with"] = TotalCharactersToWinWith
 #
+
+class percentage_trophies(Range):
+    """
+    Select what percentage (1-100) of trophies are needed to achieve the goal.
+    100 is all available trophies are required for the goal.
+    """
+    display_name = "What percentage of trophies to collect?"
+    range_start = 1
+    range_end = 100
+    default = 100
+
+class select_difficulty(Choice):
+    """
+    Select what difficulty locations are included in the randomizer.
+    Each difficulty adds three checks per race track.
+    Each difficulty adds five checks per battle track.
+    """
+    display_name = "Select Difficulties to include"
+    option_easy = 0
+    option_medium = 1
+    option_hard = 2
+    option_easy_medium = 3
+    option_easy_hard = 4
+    option_medium_hard = 5
+    option_all = 6
+    default = 2
+
 class include_single_race(DefaultOnToggle):
     """
     Choose whether to include Single Race arcade mode.
@@ -80,31 +107,25 @@ class select_battle_tracks(Choice):
     option_classic_nitro = 2
     default = 0
 
-class select_difficulty(Choice):
+class include_time_trial(Toggle):
     """
-    Select what difficulty locations are included in the randomizer.
-    Each difficulty adds three checks per race track.
-    Each difficulty adds five checks per battle track.
+    Choose whether to include Time Trial mode.
+    At least one game mode must be included to generate.
     """
-    display_name = "Select Difficulties to include"
-    option_easy = 0
-    option_medium = 1
-    option_hard = 2
-    option_easy_medium = 3
-    option_easy_hard = 4
-    option_medium_hard = 5
-    option_all = 6
-    default = 2
-
-class percentage_trophies(Range):
+    display_name = "Include Time Trial Mode?"
+    
+class included_ghosts(Choice):
     """
-    Select what percentage (1-100) of trophies are needed to achieve the goal.
-    100 is all available trophies are required for the goal.
+    Choose whether to include Time Trial ghosts in the locations pool.
+    Each ghost will also include the ghosts before it.
+    This setting does nothing if Time Trial is not included.
     """
-    display_name = "What percentage of trophies to collect?"
-    range_start = 1
-    range_end = 100
-    default = 100
+    display_name = "Select Ghosts"
+    option_n_tropy = 0
+    option_n_oxide = 1
+    option_emperor_velo_xxvii = 2
+    option_beenox_developer = 3
+    default = 1
 
 # This is called before any manual options are defined, in case you want to define your own with a clean slate or let Manual define over them
 def before_options_defined(options: dict) -> dict:
@@ -112,6 +133,12 @@ def before_options_defined(options: dict) -> dict:
     options["select_difficulty"] = select_difficulty
     options["include_single_race"] = include_single_race
     options["select_race_tracks"] = select_race_tracks
+    options["include_cups"] = include_cups
+    options["include_battle"] = include_battle
+    options["select_battle_tracks"] = select_battle_tracks
+    options["include_time_trial"] = include_time_trial
+    options["included_ghosts"] = included_ghosts
+
     return options
 
 # This is called after any manual options are defined, in case you want to see what options are defined or want to modify the defined options
