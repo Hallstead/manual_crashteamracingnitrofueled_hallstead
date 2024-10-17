4a745887289377f9e40820417520063a42cc082a
Hallstead <106173817+Hallstead@users.noreply.github.com>
2024-10-17 16:14:41 -0500

    Merge Classic CTR into back into NF

--- hooks/Options.py
+++ hooks/Options.py
@@ -27,6 +27,17 @@ from ..Helpers import is_option_enabled, get_option_value
 #   options["total_characters_to_win_with"] = TotalCharactersToWinWith
 #
 
+class ctr_game(Choice):
+    """
+    This option tells generation what game you are playing.
+    Classic CTR: This is the PS1 version of Crash Team Racing. Some options are not available with this option selected.
+    CTR_NF: This is Crash Team Racing: Nitro Fueled.
+    """
+    display_name = "What game are you playing?"
+    option_classic_ctr = 0
+    option_ctr_nf = 1
+    default = 1
+
 class goal_type(Choice):
     """
     This option determines the goal. if there is a final challenge after collecting the number of trophies.
@@ -96,6 +107,7 @@ class include_single_race(DefaultOnToggle):
 
 class select_race_tracks(Choice):
     """
+    CTR:NF only.
     Select what track sets are included in the randomizer. 
     Classic adds 18 tracks
     Nitro adds 13 tracks
@@ -112,6 +124,15 @@ class select_race_tracks(Choice):
     option_nitro_bonus = 5
     option_all = 6
     default = 0
+
+class include_turbo_track(DefaultOnToggle):
+    """
+    Choose whether to include Turbo Track in Single Race arcade mode.
+    To unlock Turbo Track in Classic CTR, you can enter the following code at the main menu:
+    Hold L1+R1 and Press Right (x2), Left, Triangle, Right, Down (x2)
+    Turbo Track is already unlocked by default in CTR:NF.
+    """
+    display_name = "Include Turbo Track?"
 
 class include_cups(Toggle):
     """
@@ -133,6 +154,7 @@ class cups_unlock_method(Choice):
 
 class include_battle(Toggle):
     """
+    CTR:NF only.
     Choose whether to include Battle mode.
     At least one game mode must be included to generate.
     """
@@ -140,6 +162,7 @@ class include_battle(Toggle):
 
 class select_battle_tracks(Choice):
     """
+    CTR:NF only.
     Select what battle sets are included in the randomizer. If using the PS1 CTR, only use Classic.
     Classic adds 7 battle tracks
     Nitro adds 5 battle tracks
@@ -164,9 +187,10 @@ class included_ghosts(Choice):
     Choose whether to include Time Trial ghosts in the locations pool.
     Each ghost will also include the ghosts before it.
     This setting does nothing if Time Trial is not included.
+    Only N. Tropy and N. Oxide are available in Classic CTR.
     """
     display_name = "Select Ghosts"
-    #option_none = 0
+    option_none = 0
     option_n_tropy = 1
     option_n_oxide = 2
     option_emperor_velo_xxvii = 3
@@ -178,6 +202,7 @@ class randomize_characters(Choice):
     Characters or Driving Stykes are added into the item pool as filler. Depending on other settings, not all randomized characters may be available in a given seed.
     None: All characters are playable as the player chooses.
     Driving Styles: All characters are playable as the player chooses, but thefive driving styles are added to thr pool and can omly be used once found.
+        This option not available in Classic CTR.
     Starter: The starter characters are randomized. Unlockable characters and characters from the pit stop are not playable in this option.
     Unlockable: Starter and Unlockable characters are randomized. Characters from the Pit Stop are not playable in this option.
     All Characters: All characters are randomized.
@@ -201,6 +226,7 @@ class oxide_edition(Toggle):
 
 # This is called before any manual options are defined, in case you want to define your own with a clean slate or let Manual define over them
 def before_options_defined(options: dict) -> dict:
+    options["ctr_game"] = ctr_game
     options["goal_type"] = goal_type
     options["percentage_trophies"] = percentage_trophies
     options["unlock_mode"] = unlock_mode
@@ -208,6 +234,7 @@ def before_options_defined(options: dict) -> dict:
     options["select_difficulty"] = select_difficulty
     options["include_single_race"] = include_single_race
     options["select_race_tracks"] = select_race_tracks
+    options["include_turbo_track"] = include_turbo_track
     options["include_cups"] = include_cups
     options["cups_unlock_method"] = cups_unlock_method
     #options["include_battle"] = include_battle
